89df9de25a5a717802290446d8f24d4809a499d0
Richard Liu <liuzhenglaichn@gmail.com>
2020-03-30 22:41:51 -0700

    highmaps

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,87 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.sass";
+import Highcharts from "highcharts/highmaps";
+import HighchartsReact from "highcharts-react-official";
+import { mapData } from "./us";
 
 function App() {
+    var data = [
+        ["us-ma", 0],
+        ["us-wa", 1],
+        ["us-ca", 2],
+        ["us-or", 3],
+        ["us-wi", 4],
+        ["us-me", 5],
+        ["us-mi", 6],
+        ["us-nv", 7],
+        ["us-nm", 8],
+        ["us-co", 9],
+        ["us-wy", 10],
+        ["us-ks", 11],
+        ["us-ne", 12],
+        ["us-ok", 13],
+        ["us-mo", 14],
+        ["us-il", 15],
+        ["us-in", 16],
+        ["us-vt", 17],
+        ["us-ar", 18],
+        ["us-tx", 19],
+        ["us-ri", 20],
+        ["us-al", 21],
+        ["us-ms", 22],
+        ["us-nc", 23],
+        ["us-va", 24],
+        ["us-ia", 25],
+        ["us-md", 26],
+        ["us-de", 27],
+        ["us-pa", 28],
+        ["us-nj", 29],
+        ["us-ny", 30],
+        ["us-id", 31],
+        ["us-sd", 32],
+        ["us-ct", 33],
+        ["us-nh", 34],
+        ["us-ky", 35],
+        ["us-oh", 36],
+        ["us-tn", 37],
+        ["us-wv", 38],
+        ["us-dc", 39],
+        ["us-la", 40],
+        ["us-fl", 41],
+        ["us-ga", 42],
+        ["us-sc", 43],
+        ["us-mn", 44],
+        ["us-mt", 45],
+        ["us-nd", 46],
+        ["us-az", 47],
+        ["us-ut", 48]
+    ];
+
+    const options: Highcharts.Options = {
+        title: {
+            text: "My chart"
+        },
+
+        colorAxis: {
+            min: 0
+        },
+
+        series: [
+            {
+                mapData: mapData,
+                name: "Random data",
+                data: data
+            } as any
+        ]
+    };
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    options={options}
+                    constructorType={"mapChart"}
+                />
             </header>
         </div>
     );
